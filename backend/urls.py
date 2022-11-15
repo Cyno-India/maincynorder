8b7d95be443a8d0beb675d9e3c008a727f0e0f03
Cyno India <cyno@Cynos-MacBook-Air.local>
2022-11-15 14:18:45 +0530

    mail added

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view()),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('api/order/', include('order.urls')),
 
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
